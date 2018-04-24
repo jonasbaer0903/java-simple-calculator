7a44c9f54709cbad8ce9e579f9e6cbd68c31237d
Jonas Bär <jonasbaer0903@gmail.com>
2018-04-24 11:25:00 +0200

    Reformat code

--- src/com/kaufland/hello/CommandLine.java
+++ src/com/kaufland/hello/CommandLine.java
@@ -9,23 +9,23 @@ public class CommandLine {
         String eingabe2;
         String eingabeRechenart;
 
-        eingabe1= readLine("Gib Zahl 1 ein: ");
-        eingabe2= readLine("Gib Zahl 2 ein: ");
-        eingabeRechenart=readLine("Gib Rechenart ein: ");
+        eingabe1 = readLine("Gib Zahl 1 ein: ");
+        eingabe2 = readLine("Gib Zahl 2 ein: ");
+        eingabeRechenart = readLine("Gib Rechenart ein: ");
 
         double zahl1 = Double.parseDouble(eingabe1);
         double zahl2 = Double.parseDouble(eingabe2);
 
         double result = 0;
 
-        if (eingabeRechenart.equals("+")){
-            result = add(zahl1,zahl2);
-        } else if (eingabeRechenart.equals("-")){
-            result = subtract(zahl1,zahl2);
-        }else if (eingabeRechenart.equals("/")){
-            result = devide(zahl1,zahl2);
-        }else {
-            result = multiply(zahl1,zahl2);
+        if (eingabeRechenart.equals("+")) {
+            result = add(zahl1, zahl2);
+        } else if (eingabeRechenart.equals("-")) {
+            result = subtract(zahl1, zahl2);
+        } else if (eingabeRechenart.equals("/")) {
+            result = devide(zahl1, zahl2);
+        } else {
+            result = multiply(zahl1, zahl2);
         }
 
 
@@ -33,17 +33,19 @@ public class CommandLine {
     }
 
 
-
-    private static double add(double zahl1, double zahl2){
+    private static double add(double zahl1, double zahl2) {
         return zahl1 + zahl2;
     }
-    private static double subtract(double zahl1, double zahl2){
+
+    private static double subtract(double zahl1, double zahl2) {
         return zahl1 - zahl2;
     }
-    private static double devide(double zahl1, double zahl2){
+
+    private static double devide(double zahl1, double zahl2) {
         return zahl1 / zahl2;
     }
-    private static double multiply(double zahl1, double zahl2){
+
+    private static double multiply(double zahl1, double zahl2) {
         return zahl1 * zahl2;
     }
 
--- src/com/kaufland/hello/Main.java
+++ src/com/kaufland/hello/Main.java
@@ -1,8 +1,10 @@
 package com.kaufland.hello;
-import java.util.*;
-import java.awt.*;
+
 import javax.swing.*;
-import java.awt.event.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,10 +14,8 @@ public class Main {
     }
 }
 
-class CalculatorFrame extends JFrame
-{
-    public CalculatorFrame()
-    {
+class CalculatorFrame extends JFrame {
+    public CalculatorFrame() {
         setTitle("Calculator");
         CalculatorPanel panel = new CalculatorPanel();
         add(panel);
@@ -25,6 +25,11 @@ class CalculatorFrame extends JFrame
 
 
 class CalculatorPanel extends JPanel {
+    private JButton display;
+    private JPanel panel;
+    private boolean start;
+    private double result;
+    private String lastCommand;
     public CalculatorPanel() {
 
         setLayout(new BorderLayout());
@@ -43,55 +48,48 @@ class CalculatorPanel extends JPanel {
         panel = new JPanel();
         panel.setLayout(new GridLayout(4, 4));
         // Generierung der numerischen Taschenrechner-Tasten
-        addButton("7",insert);
-        addButton("8",insert);
-        addButton("9",insert);
+        addButton("7", insert);
+        addButton("8", insert);
+        addButton("9", insert);
         addButton("/", command);
 
-        addButton("4",insert);
-        addButton("5",insert);
-        addButton("6",insert);
+        addButton("4", insert);
+        addButton("5", insert);
+        addButton("6", insert);
         addButton("*", command);
 
-        addButton("1",insert);
-        addButton("2",insert);
-        addButton("3",insert);
+        addButton("1", insert);
+        addButton("2", insert);
+        addButton("3", insert);
         addButton("-", command);
 
-        addButton("0",insert);
-        addButton(".",insert);
+        addButton("0", insert);
+        addButton(".", insert);
         addButton("=", command);
         addButton("+", command);
 
         add(panel, BorderLayout.CENTER);
     }
 
-    private void addButton(String label, ActionListener listener)
-    {
+    private void addButton(String label, ActionListener listener) {
         JButton button = new JButton(label);
         button.addActionListener(listener);
         panel.add(button);
     }
-    private class CommandAction implements ActionListener
-    {
-        public void actionPerformed(ActionEvent event)
-        {
+
+    private class CommandAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
             String command = event.getActionCommand();
 
             // Füge den Präfix "-" an den String an wenn
             // es sich um den ersten Befehl handelt (negative Zahl)
-            if (start)
-            {
-                if (command.equals("-"))
-                {
+            if (start) {
+                if (command.equals("-")) {
                     display.setText(command);
                     start = false;
-                }
-                else
+                } else
                     lastCommand = command;
-            }
-            else
-            {
+            } else {
                 // Berechnung ausführen
                 //calculate(Double.parseDouble(display.getText()));
                 lastCommand = command;
@@ -99,15 +97,13 @@ class CalculatorPanel extends JPanel {
             }
         }
     }
-    private class InsertAction implements ActionListener
-    {
-        public void actionPerformed(ActionEvent event)
-        {
+
+    private class InsertAction implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
             String input = event.getActionCommand();
             // Beim Start keinen Text anzeigen
             // Das Start Flag anschließend auf false setzen
-            if (start)
-            {
+            if (start) {
                 display.setText("");
                 start = false;
             }
@@ -115,9 +111,4 @@ class CalculatorPanel extends JPanel {
             display.setText(display.getText() + input);
         }
     }
-    private JButton display;
-    private JPanel panel;
-    private boolean start;
-    private double result;
-    private String lastCommand;
 }
